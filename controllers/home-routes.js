b11f60b16b972637f9706f3784f14919cfdaebd1
Kilisi <kilisivazquez@yahoo.com>
2022-08-04 19:47:24 -0700

    more code for the home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,73 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    try{
-        res.render('homepage', {
-        
-        });
-    }catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-}
+  try {
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const projects = projectData.map((project) => project.get({ plain: true }));
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-//// GET route for getting all of the dishes that are on the menu
-//router.get('/', async (req, res) => {
-//// This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-  //res.render('all');
-//});
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//get login template, this method renders the 'login' template
+    const project = projectData.get({ plain: true });
 
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
