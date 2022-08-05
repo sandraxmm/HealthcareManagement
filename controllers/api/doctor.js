cca3b3dec4a5c284926ad19af8d340e663f0f5e4
Kilisi <kilisivazquez@yahoo.com>
2022-08-04 21:37:02 -0700

    fixed the Doctor.js

--- controllers/api/doctor.js
+++ controllers/api/doctor.js
@@ -4,38 +4,61 @@ const User = require('../../models/Doctor');
 const withAuth = require('../../utils/auth');
 const { Doctor } = require('../../models');
 
-router.post('/', withAuth, async (req, res) => {
-    try {
-      const newDoctor = await Doctor.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newDoctor);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  router.delete('/:id', withAuth, async (req, res) => {
-    try {
-      const doctorData = await Doctor.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!doctorData) {
-        res.status(404).json({ message: 'No Doctor found with this id!' });
-        return;
-      }
-  
+router.post('/', async (req, res) => {
+  try {
+    const doctorData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = doctorData.id;
+      req.session.logged_in = true;
+
       res.status(200).json(doctorData);
-    } catch (err) {
-      res.status(500).json(err);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const doctorData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!doctorData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
-  });
-  
-  module.exports = router;
-  
+
+    const validPassword = await doctorData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = doctorData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: doctorData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
